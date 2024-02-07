303395c6828f5927639f5bc7cfd359d60753cffc
nathanlafeniere <nathan.lafreniere.01@edu.cegeptr.qc.ca>
2024-02-07 09:23:20 -0500

    nav build main

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_prise_de_donnee, R.id.navigation_statistique, R.id.navigation_notifications)
+                R.id.navigation_prise_de_donnee, R.id.navigation_statistique, R.id.navigation_notifications, R.id.navigation_infoSanté, R.id.navigation_profil)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
