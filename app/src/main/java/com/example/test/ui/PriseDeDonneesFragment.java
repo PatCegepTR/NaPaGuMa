379e25b7d1978e5ae88d60ca15967ba4fc01d16b
nathanlafeniere <nathan.lafreniere.01@edu.cegeptr.qc.ca>
2024-02-07 09:20:34 -0500

    test navigation debug

--- app/src/main/java/com/example/test/ui/PriseDeDonneesFragment.java
+++ app/src/main/java/com/example/test/ui/PriseDeDonneesFragment.java
@@ -1,4 +1,4 @@
-package com.example.test;
+package com.example.test.ui;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.test.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link StatistiquesFragment#newInstance} factory method to
+ * Use the {@link PriseDeDonneesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class StatistiquesFragment extends Fragment {
+public class PriseDeDonneesFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class StatistiquesFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public StatistiquesFragment() {
+    public PriseDeDonneesFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class StatistiquesFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment StatistiquesFragment.
+     * @return A new instance of fragment PriseDeDonneesFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static StatistiquesFragment newInstance(String param1, String param2) {
-        StatistiquesFragment fragment = new StatistiquesFragment();
+    public static PriseDeDonneesFragment newInstance(String param1, String param2) {
+        PriseDeDonneesFragment fragment = new PriseDeDonneesFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class StatistiquesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistiques, container, false);
+        return inflater.inflate(R.layout.fragment_prise_de_donnees, container, false);
     }
 }
