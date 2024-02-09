29ed206e25633e8a0f8a77aa11415f71daaae0b4
nathanlafeniere <nathan.lafreniere.01@edu.cegeptr.qc.ca>
2024-02-09 10:35:06 -0500

    test interface

---
+++ app/src/main/java/com/example/test/Profil/AdapterProfil.java
@@ -0,0 +1,12 @@
+package com.example.test.Profil;
+
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.test.R;
+
+public class AdapterProfil {
+
+}
