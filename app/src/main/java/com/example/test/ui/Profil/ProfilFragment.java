7faf3161e7c62dc5c6c7b4555081ab116ed947ff
Nathan Lafrenière <nathan.lafreniere.01@edu.cegeptr.qc.ca>
2024-02-16 11:00:27 -0500

    Update ProfilFragment.java

--- app/src/main/java/com/example/test/ui/Profil/ProfilFragment.java
+++ app/src/main/java/com/example/test/ui/Profil/ProfilFragment.java
@@ -20,48 +20,20 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfilFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ProfilFragment extends Fragment implements InterfaceProfil {
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    private String mParam1;
-    private String mParam2;
 
+public class ProfilFragment extends Fragment implements InterfaceProfil {
     TextView tvInfoProfils;
 
     public ProfilFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfilFragment newInstance(String param1, String param2) {
-        ProfilFragment fragment = new ProfilFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -70,10 +42,11 @@ public class ProfilFragment extends Fragment implements InterfaceProfil {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_profil, container, false);
 
-        TextView infoProfil = view.findViewById(R.id.tvInfoPersoProfil);
+
         ImageView imgProfil = view.findViewById(R.id.ivProfil);
 
-        //infoProfil.setText("Nom : " + "Dupont" + "\n" + "Prénom : " + "Jean" + "\n" + "Date de naissance : " + "01/01/2000" + "\n" + "Adresse mail : ");
+        getProfilById();
+
         imgProfil.setImageResource(R.drawable.ic_launcher_foreground);
 
         return view;
