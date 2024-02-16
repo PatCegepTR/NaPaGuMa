2e993e3b81f11c4f5264d67dedcdb4207413b27c
PatCegepTR <patrice.anctil.01@edu.cegeptr.qc.ca>
2024-02-16 10:10:11 -0500

    Interface appel getProfil()

--- app/src/main/java/com/example/test/InterfaceServeur.java
+++ app/src/main/java/com/example/test/InterfaceServeur.java
@@ -8,10 +8,11 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface InterfaceServeur {
-    @GET("/H2024/420616RI/GR01/p_anctil/getDonnees.php")
-    Call<String> getBonjour();
-
 
     @GET("/H2024/420616RI/GR01/p_anctil/getDonnees2.php")
     Call<List<LesDonnees>> getDonnees2();
+
+    @GET("/H2024/420616RI/GR01/p_anctil/getProfil.php")
+    Call<List<LesDonnees>> getProfil();
+
 }
