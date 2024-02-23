0b9742c4c19ef2e594a4bb0bd5a72ed810e3f965
Guillaume Blier <thedreamyport@hotmail.com>
2024-02-23 09:25:27 -0500

    Info Sante.
    Création de la View avec son RecyclerView.

--- app/src/main/java/com/example/test/ui/InfoSante/InfoSanteFragment.java
+++ app/src/main/java/com/example/test/ui/InfoSante/InfoSanteFragment.java
@@ -1,22 +1,34 @@
 package com.example.test.ui.InfoSante;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.test.LesDonnees;
 import com.example.test.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link InfoSanteFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class InfoSanteFragment extends Fragment {
+public class InfoSanteFragment extends Fragment implements InterfaceInfoSante{
+
+    RecyclerView recyclerViewInfoSante;
+    AdapterInfoSante adapterInfoSante;
+    List<InfoSante> listeArticlesInfoSante = new ArrayList<InfoSante>();
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -64,8 +76,29 @@ public class InfoSanteFragment extends Fragment {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_info_sante, container, false);
 
-        RecyclerView recyclerView = view.findViewById(R.id.rvInfoSante);
-
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Context context = getContext();
+
+        recyclerViewInfoSante = view.findViewById(R.id.rvInfoSante);
+        recyclerViewInfoSante.setHasFixedSize(true);
+        recyclerViewInfoSante.setLayoutManager(new LinearLayoutManager(context));
+
+        listeArticlesInfoSante.add(new InfoSante("titre", "02-02-23", "https://media.istockphoto.com/id/1383851086/fr/vectoriel/ic%C3%B4ne-de-ligne-de-battement-de-c%C5%93ur-rouge-moniteur-de-fr%C3%A9quence-du-pouls-sur-fond-blanc.jpg?s=612x612&w=0&k=20&c=s8Nm82KXvUYf4P-yJcHzIbWeuC2-mYgqAacZT_K9Ih4=", "www.google.ca", "Je parle du poul."));
+        listeArticlesInfoSante.add(new InfoSante("titre", "02-02-23", "https://media.istockphoto.com/id/1383851086/fr/vectoriel/ic%C3%B4ne-de-ligne-de-battement-de-c%C5%93ur-rouge-moniteur-de-fr%C3%A9quence-du-pouls-sur-fond-blanc.jpg?s=612x612&w=0&k=20&c=s8Nm82KXvUYf4P-yJcHzIbWeuC2-mYgqAacZT_K9Ih4=", "www.google.ca", "Je parle du poul."));
+        listeArticlesInfoSante.add(new InfoSante("titre", "02-02-23", "https://media.istockphoto.com/id/1383851086/fr/vectoriel/ic%C3%B4ne-de-ligne-de-battement-de-c%C5%93ur-rouge-moniteur-de-fr%C3%A9quence-du-pouls-sur-fond-blanc.jpg?s=612x612&w=0&k=20&c=s8Nm82KXvUYf4P-yJcHzIbWeuC2-mYgqAacZT_K9Ih4=", "www.google.ca", "Je parle du poul."));
+
+        adapterInfoSante = new AdapterInfoSante(listeArticlesInfoSante, this);
+
+        recyclerViewInfoSante.setAdapter(adapterInfoSante);
+
+    }
+
+    public void GestionClicArticle(int position, InfoSante article){
+
     }
 }
