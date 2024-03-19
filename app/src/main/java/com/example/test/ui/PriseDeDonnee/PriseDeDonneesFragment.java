55fb20a8c982ca5dc64ac371fa8823b6058f8eef
PatCegepTR <patrice.anctil.01@edu.cegeptr.qc.ca>
2024-03-19 14:45:55 -0400

    prisedonnees

--- app/src/main/java/com/example/test/ui/PriseDeDonnee/PriseDeDonneesFragment.java
+++ app/src/main/java/com/example/test/ui/PriseDeDonnee/PriseDeDonneesFragment.java
@@ -2,11 +2,14 @@ package com.example.test.ui.PriseDeDonnee;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.test.R;
 
@@ -14,6 +17,7 @@ import com.example.test.R;
 public class PriseDeDonneesFragment extends Fragment {
 
 
+    Button priseDonnee;
     public PriseDeDonneesFragment() {
         // Required empty public constructor
     }
@@ -28,5 +32,19 @@ public class PriseDeDonneesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_prise_de_donnees, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        priseDonnee = view.findViewById(R.id.fbCommencerPrise);
+
+        priseDonnee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
